6d8ed31f28e580eac2ea0f8916aceddd90623dcd
secdie <48974647+diemasmahendra@users.noreply.github.com>
2023-02-03 19:26:35 +0700

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const array = [1, 2, 3, 4, 6, 5, 7, 8, 9, 10, 11, 13, 12, 14, 15, 16, 17, 18, 19, 20];
-const middle = Math.floor(array.length / 2);
-
-for (let i = 0; i < middle; i++) {
-  console.log(`${array[i]} -> ${array[array.length - 1 - i]} -> ${array[i]}`);
-}
